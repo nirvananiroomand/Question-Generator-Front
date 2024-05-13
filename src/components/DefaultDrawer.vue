61bcfcddf22a7e19798d1dd64bf22fc76f3afd0f
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-13 14:16:41 +0330

    add fetchChatsData function

--- src/components/DefaultDrawer.vue
+++ src/components/DefaultDrawer.vue
@@ -4,7 +4,7 @@
       <v-list-item>
         <v-list-item-title class="text-blue text-h5">History</v-list-item-title>
       </v-list-item>
-      <v-divider/>
+      <v-divider />
     </template>
 
     <v-list>
@@ -24,102 +24,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'DefaultDrawer',
   data() {
     return {
-      navigationItems: [
-        {
-          id: 1,
-          title: 'First Chat',
-          created_at: '2024-05-05T17:00:26.443737Z'
-        },
-        {
-          id: 2,
-          title: 'Second Chat',
-          created_at: '2024-05-05T17:00:56.794126Z'
-        },
-        {
-          id: 3,
-          title: 'Chat number 3',
-          created_at: '2024-05-05T17:01:32.860301Z'
-        },
-        {
-          id: 4,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:35:31.291781Z'
-        },
-        {
-          id: 5,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:37:43.324498Z'
-        },
-        {
-          id: 6,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:38:02.834822Z'
-        },
-        {
-          id: 7,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:10:30.781825Z'
-        },
-        {
-          id: 8,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:18:29.740402Z'
-        },
-        {
-          id: 9,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:35:56.173641Z'
-        },
-        {
-          id: 11,
-          title: 'First Chat',
-          created_at: '2024-05-05T17:00:26.443737Z'
-        },
-        {
-          id: 12,
-          title: 'Second Chat',
-          created_at: '2024-05-05T17:00:56.794126Z'
-        },
-        {
-          id: 13,
-          title: 'Chat number 3',
-          created_at: '2024-05-05T17:01:32.860301Z'
-        },
-        {
-          id: 14,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:35:31.291781Z'
-        },
-        {
-          id: 15,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:37:43.324498Z'
-        },
-        {
-          id: 16,
-          title: 'title of this chat',
-          created_at: '2024-05-07T11:38:02.834822Z'
-        },
-        {
-          id: 17,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:10:30.781825Z'
-        },
-        {
-          id: 18,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:18:29.740402Z'
-        },
-        {
-          id: 19,
-          title: 'title of this chat',
-          created_at: '2024-05-07T15:35:56.173641Z'
-        }
-      ]
+      navigationItems: []
+    }
+  },
+  mounted() {
+    this.fetchChatsData()
+  },
+  methods: {
+    async fetchChatsData() {
+      console.log("in fetch chats data")
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/conversations/chats/')
+        this.navigationItems = response.data.chats
+        console.log(this.navigationItems)
+      } catch (error) {
+        console.error('Error fetching chats:', error)
+      }
     }
   }
 }
