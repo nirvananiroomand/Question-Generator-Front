01c059ca4963228f5668618d6335060fd7f83f47
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-27 15:39:40 +0330

    Add authorization header in axios requests

--- src/store/chat/index.js
+++ src/store/chat/index.js
@@ -30,27 +30,45 @@ export default {
     async getAllChats(context) {
       context.commit('setIsRetrievingChatsHistory', true)
       try {
-        const response = await axios.get('http://127.0.0.1:8000/conversations/chats/')
+        const accessToken = context.rootGetters['user/accessToken']
+        const response = await axios.get('http://127.0.0.1:8000/conversations/chats/', {
+          headers: {
+            Authorization: `JWT ${accessToken}`
+          }
+        })
         context.commit('setIsRetrievingChatsHistory', false)
         context.commit('setChats', response.data.chats)
       } catch (error) {
         context.commit('setIsRetrievingChatsHistory', false)
+        console.error('Error fetching chats:', error)
       }
     },
     async createChat(context, payload) {
+      context.commit('setIsGenerating', true)
       try {
-        context.commit('setIsGenerating', true)
-        const response = await axios.post('http://127.0.0.1:8000/conversations/chats/', payload)
+        const accessToken = context.rootGetters['user/accessToken']
+        const response = await axios.post('http://127.0.0.1:8000/conversations/chats/', payload, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `JWT ${accessToken}`
+          }
+        })
         context.commit('addNewChat', response.data)
         context.commit('setIsGenerating', false)
         return response.data
       } catch (error) {
+        console.error('Error generating questions:', error.message)
         context.commit('setIsGenerating', false)
         throw error
       }
     },
     async getChatDetail(context, id) {
-      const response = await axios.get(`http://127.0.0.1:8000/conversations/chats/${id}`)
+      const accessToken = context.rootGetters['user/accessToken']
+      const response = await axios.get(`http://127.0.0.1:8000/conversations/chats/${id}`, {
+        headers: {
+          Authorization: `JWT ${accessToken}`
+        }
+      })
       return response.data
     }
   },
